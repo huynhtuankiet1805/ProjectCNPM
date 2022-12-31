d47c8e6366a432bfa8609a554be8b84ed2984884
phuonguyen11 <phuonguyen11112002@gmail.com>
2022-12-31 10:20:46 +0700

    init helper libraries

--- src/pages/Orderfood/index.js
+++ src/pages/Orderfood/index.js
@@ -1,8 +1,35 @@
 import './order.scss'
-function Order() {
-    return <div>
-        <h2> Trang order ở đây</h2>
-    </div>;
+import { Button} from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import BannerImg from './../../image/banner_img.jpg'
+
+const Order = () => {
+    return (
+    <section>
+            <Container>
+            <Col lg = '6' md = '6'>
+                        <div className = "banner_img">
+                            <img src = {BannerImg} alt ="bannerimg">
+                                
+                            </img>
+
+                        </div>
+                    </Col>
+
+
+                <Row>
+                    <Col lg = '6' md = '6'>
+                        <div className = "banner_content">
+                            <h1 className = "Bu">Bu</h1>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
+    </section>
+    );
 }
 
 export default Order;
